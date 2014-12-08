dec82ba86aacf016815e55cd42d19c62c2a01244
jay whitsitt <jay.whitsitt@hallmark.com>
2014-12-07 18:15:11 -0600

    Switched to nibs instead of storyboard

--- Etsy Search/AppDelegate.m
+++ Etsy Search/AppDelegate.m
@@ -8,6 +8,8 @@
 
 #import "AppDelegate.h"
 
+#import "SearchViewController.h"
+
 @interface AppDelegate ()
 
 @end
@@ -17,6 +19,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor blackColor];
+    [self.window makeKeyAndVisible];
+    
+    SearchViewController *searchViewController = [[SearchViewController alloc] init];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:searchViewController];
+    
     return YES;
 }
 
