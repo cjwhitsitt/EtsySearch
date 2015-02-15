70ae35f5087d4a7d9eeddb55c85b11836485c69c
Jay Whitsitt <jay.whitsitt@gmail.com>
2015-02-15 01:10:42 -0600

    Added ResultDetailViewController

---
+++ Etsy Search/View Controllers/ResultDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  ResultDetailViewController.h
+//  Etsy Search
+//
+//  Created by Jay Whitsitt on 2/15/15.
+//  Copyright (c) 2015 Jay Whitsitt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ResultDetailViewController : UIViewController
+
+@end
---
+++ Etsy Search/View Controllers/ResultDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  ResultDetailViewController.m
+//  Etsy Search
+//
+//  Created by Jay Whitsitt on 2/15/15.
+//  Copyright (c) 2015 Jay Whitsitt. All rights reserved.
+//
+
+#import "ResultDetailViewController.h"
+
+@interface ResultDetailViewController ()
+
+@end
+
+@implementation ResultDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
