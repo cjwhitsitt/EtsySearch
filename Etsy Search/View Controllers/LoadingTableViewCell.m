2a58000fea978cb94ddc868e8dccbf4ff3cd743d
Jay Whitsitt <jay.whitsitt@gmail.com>
2015-02-16 00:29:35 -0600

    Cleaned up the loading cell states

--- Etsy Search/View Controllers/LoadingTableViewCell.m
+++ Etsy Search/View Controllers/LoadingTableViewCell.m
@@ -10,11 +10,7 @@
 
 @interface LoadingTableViewCell ()
 
-@property (weak, nonatomic) IBOutlet UILabel *pullDownInstructionLabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *letGoInstructionlabel;
-
-@property (weak, nonatomic) IBOutlet UILabel *loadingLabel;
+@property (weak, nonatomic) IBOutlet UILabel *instructionLabel;
 @property (weak, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicator;
 
 @end
@@ -25,33 +21,22 @@
 
 - (void)reset
 {
-    // what should be done here?
-    [self shouldBeLoading:NO];
-    self.pullDownInstructionLabel.hidden = NO;
-    self.letGoInstructionlabel.hidden = YES;
+    self.instructionLabel.text = @"Pull down to load more";
+    [self.activityIndicator stopAnimating];
 }
 
 - (void)indicateThresholdReached
 {
-    [self shouldBeLoading:NO];
-    self.pullDownInstructionLabel.hidden = YES;
-    self.letGoInstructionlabel.hidden = NO;
+    self.instructionLabel.text = @"Let go to learn more";
 }
 
 - (void)startLoading
 {
-    [self shouldBeLoading:YES];
-    self.pullDownInstructionLabel.hidden = YES;
-    self.letGoInstructionlabel.hidden = YES;
+    self.instructionLabel.text = @"Loading...";
+    [self.activityIndicator startAnimating];
 }
 
 #pragma mark - Private methods
-
-- (void)shouldBeLoading:(BOOL)shouldBeLoading
-{
-    self.loadingLabel.hidden = !shouldBeLoading;
-    (shouldBeLoading ? [self.activityIndicator startAnimating] : [self.activityIndicator stopAnimating]);
-}
 
 - (void)awakeFromNib {
     // Initialization code
