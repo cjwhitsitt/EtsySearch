2e56e853045666c1f9847c1dcfc08b3ebe8ab699
Jay Whitsitt <jay.whitsitt@gmail.com>
2017-04-25 00:55:23 -0500

    Updated for iOS 10

--- Etsy Search/View Controllers/LoadingTableViewCell.m
+++ Etsy Search/View Controllers/LoadingTableViewCell.m
@@ -37,16 +37,4 @@
     [self.activityIndicator startAnimating];
 }
 
-#pragma mark - Private methods
-
-- (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-    
-    // Configure the view for the selected state
-}
-
 @end
--- Etsy Search/View Controllers/ResultTableViewCell.m
+++ Etsy Search/View Controllers/ResultTableViewCell.m
@@ -10,14 +10,4 @@
 
 @implementation ResultTableViewCell
 
-- (void)awakeFromNib {
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
-    [super setSelected:selected animated:animated];
-
-    // Configure the view for the selected state
-}
-
 @end
